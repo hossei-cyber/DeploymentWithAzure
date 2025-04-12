d786d25176e8038f7fe1c4a9d807eb1df731e69b
hossei-cyber <hussayaqobzai@gmail.com>
2025-04-12 10:37:52 +0200

    changing the port an dthe link to web app

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const app = express();
 //   }
 // });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
