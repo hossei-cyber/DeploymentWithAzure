09f4428c9684b10ef21d2705472f3efb9f701f90
hossei-cyber <hussayaqobzai@gmail.com>
2025-04-12 16:20:30 +0200

    removed public folder

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 // const { Pool } = require('pg');
 require('dotenv').config();
-app.use(express.static(path.join(__dirname, 'public')));
 
 // App
 const app = express();
